6ee370f4fd4e87a8d9fc0f1de7e0959f213a6af5
Grishan <grishan.rocks@gmail.com>
2026-09-01 00:50:53 +0545

    removed unwanted files

--- prisma/migrations/fix_review_unique_constraint.sql
+++
@@ -1,1 +0,0 @@
-DROP INDEX IF EXISTS "reviews_product_id_user_id_key";
--- prisma/migrations/list_constraints.sql
+++
@@ -1,4 +0,0 @@
-SELECT conname, pg_get_constraintdef(oid)
-FROM pg_constraint
-WHERE conrelid = 'reviews'::regclass
-AND contype = 'u';
